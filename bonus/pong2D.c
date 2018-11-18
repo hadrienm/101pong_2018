f6574de39cfc78dfcabf898706b695cfb822393c
Héros Adrien <Juliettead@linux.home>
2018-11-18 11:42:19 +0100

    indenting space modification

--- bonus/pong2D.c
+++ bonus/pong2D.c
@@ -20,71 +20,71 @@
 #include <stdio.h>
 
 int main()
- {
-  sfVideoMode mode = {1280, 720, 32};
-   sfRenderWindow* window;
-   sfTexture* texture;
-   sfTexture* texture2;
-   sfTexture* texture3;
-   sfSprite* sprite;
-   sfSprite* sprite2;
-   sfSprite* sprite3;
-   sfEvent event;
-   sfMusic* music;
-   sfMusic* musics;
-   int a = 1;
-   int d = 1;
-   int score = 0;
+{
+    sfVideoMode mode = {1280, 720, 32};
+    sfRenderWindow* window;
+    sfTexture* texture;
+    sfTexture* texture2;
+    sfTexture* texture3;
+    sfSprite* sprite;
+    sfSprite* sprite2;
+    sfSprite* sprite3;
+    sfEvent event;
+    sfMusic* music;
+    sfMusic* musics;
+    int a = 1;
+    int d = 1;
+    int score = 0;
 
-   sfVector2f toto = {12, 0};
-   sfVector2f tata = {1240, 0};
-   sfVector2f balle = {60, 60};
+    sfVector2f toto = {12, 0};
+    sfVector2f tata = {1240, 0};
+    sfVector2f balle = {60, 60};
 
-   window = sfRenderWindow_create(mode, "SFML window", sfResize | sfClose, NULL);
-     texture = sfTexture_createFromFile("rectangle.bmp", NULL);
-     texture2 = sfTexture_createFromFile("rectangle.bmp", NULL);
-     texture3 = sfTexture_createFromFile("balle.bmp", NULL);
+    window = sfRenderWindow_create(mode, "SFML window", sfResize | sfClose, NULL);
+    texture = sfTexture_createFromFile("rectangle.bmp", NULL);
+    texture2 = sfTexture_createFromFile("rectangle.bmp", NULL);
+    texture3 = sfTexture_createFromFile("balle.bmp", NULL);
 
-     music = sfMusic_createFromFile("balle.ogg");
-     musics = sfMusic_createFromFile("osu.ogg");
+    music = sfMusic_createFromFile("balle.ogg");
+    musics = sfMusic_createFromFile("osu.ogg");
 
-     sprite = sfSprite_create();
-     sprite2 = sfSprite_create();
-     sprite3 = sfSprite_create();
+    sprite = sfSprite_create();
+    sprite2 = sfSprite_create();
+    sprite3 = sfSprite_create();
 
-     sfSprite_setTexture(sprite, texture, sfTrue);
-     sfSprite_setTexture(sprite2, texture2, sfTrue);
-     sfSprite_setTexture(sprite3, texture3, sfTrue);
+    sfSprite_setTexture(sprite, texture, sfTrue);
+    sfSprite_setTexture(sprite2, texture2, sfTrue);
+    sfSprite_setTexture(sprite3, texture3, sfTrue);
 
-     sfSprite_setPosition(sprite, toto);
-     sfSprite_setPosition(sprite2, tata);
-     sfSprite_setPosition(sprite3, balle);
+    sfSprite_setPosition(sprite, toto);
+    sfSprite_setPosition(sprite2, tata);
+    sfSprite_setPosition(sprite3, balle);
 
-     sfMusic_play(musics);
+    sfMusic_play(musics);
 
-     while (sfRenderWindow_isOpen(window)) {
-       if (balle.x != -180 && balle.x != 1170) {
-         sfRenderWindow_clear(window, sfWhite);
-         sfSprite_setPosition(sprite3, balle);
-         sfRenderWindow_drawSprite(window, sprite3, NULL);
+    while (sfRenderWindow_isOpen(window)) {
+        if (balle.x != -180 && balle.x != 1170) {
+            sfRenderWindow_clear(window, sfWhite);
+            sfSprite_setPosition(sprite3, balle);
+            sfRenderWindow_drawSprite(window, sprite3, NULL);
             if(toto.y < 570) {
-               sfSprite_setPosition(sprite, toto);
-               sfRenderWindow_drawSprite(window, sprite, NULL);
-               toto.y = balle.y;
-             }
+                sfSprite_setPosition(sprite, toto);
+                sfRenderWindow_drawSprite(window, sprite, NULL);
+                toto.y = balle.y;
+            }
 
-             if(tata.y < 570) {
-                 sfSprite_setPosition(sprite2, tata);
-                 sfRenderWindow_drawSprite(window, sprite2, NULL);
-                 tata.y = balle.y;
+            if(tata.y < 570) {
+                sfSprite_setPosition(sprite2, tata);
+                sfRenderWindow_drawSprite(window, sprite2, NULL);
+                tata.y = balle.y;
             }
             if (balle.y == -200) {
                 a = 1;
                 sfMusic_play(music);
               }
             if (balle.y == 500) {
-              a = -1;
-              sfMusic_play(music);
+                a = -1;
+                sfMusic_play(music);
             }
             if (balle.x == 1000 && balle.y == tata.y) {
                 d = -1;
@@ -94,26 +94,26 @@ int main()
                 d = 1;
                 sfMusic_play(music);
                 score++;
-              }
-              balle.x = (balle.x + 1 * d);
-              balle.y = (balle.y + 1 * a);
+            }
+            balle.x = (balle.x + 1 * d);
+            balle.y = (balle.y + 1 * a);
 
-        sfRenderWindow_drawSprite(window, sprite3, NULL);
-        sfRenderWindow_drawSprite(window, sprite, NULL);
-        sfRenderWindow_drawSprite(window, sprite2, NULL);
-        sfRenderWindow_display(window);
-        printf("mon score est de %d\n", score);
-      } else {
-        sfRenderWindow_close(window);
-      }
-  }
-  sfMusic_destroy(music);
-  sfMusic_destroy(musics);
-  sfSprite_destroy(sprite);
-  sfSprite_destroy(sprite2);
-  sfSprite_destroy(sprite3);
-  sfTexture_destroy(texture);
-  sfTexture_destroy(texture2);
-  sfTexture_destroy(texture3);
- sfRenderWindow_destroy(window);
+            sfRenderWindow_drawSprite(window, sprite3, NULL);
+            sfRenderWindow_drawSprite(window, sprite, NULL);
+            sfRenderWindow_drawSprite(window, sprite2, NULL);
+            sfRenderWindow_display(window);
+            printf("mon score est de %d\n", score);
+        } else {
+            sfRenderWindow_close(window);
+        }
+    }
+    sfMusic_destroy(music);
+    sfMusic_destroy(musics);
+    sfSprite_destroy(sprite);
+    sfSprite_destroy(sprite2);
+    sfSprite_destroy(sprite3);
+    sfTexture_destroy(texture);
+    sfTexture_destroy(texture2);
+    sfTexture_destroy(texture3);
+    sfRenderWindow_destroy(window);
 }
